2d212e094d7b3033d4777fa1cfbbf12d5dafd7e5
ananyagupta44 <ananyag.0801@gmail.com>
2026-06-19 16:13:15 +0530

    final changes

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5001/api",
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,7 +1,13 @@
+// src/lib/socket.js
+
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5001", {
-  autoConnect: false,
-});
+const socket = io(
+  process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:5001",
+  {
+    autoConnect: false,
+    transports: ["websocket"],
+  },
+);
 
 export default socket;
